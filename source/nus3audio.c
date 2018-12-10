b194c7967def28efa60506a79f2bde513dde74df
jam1garner <jam1.mcleod@hotmail.com>
2018-12-10 15:56:05 -0500

    Add minor file reading documentation

--- source/nus3audio.c
+++ source/nus3audio.c
@@ -5,11 +5,13 @@
 #include "nus3audio_file.h"
 
 Nus3audioFile* parse_file(FILE* file){
+    // Read nus3bank/nus3audio header (just some magic and the filesize)
     SectionHeader* nus3Header = malloc(sizeof(SectionHeader));
     fread(nus3Header, sizeof(SectionHeader), 1, file);
     assert(!memcmp(nus3Header, "NUS3", 4));
     free(nus3Header);
 
+    // Read basic track information (size of rest of file, )
     AudioIndexSection* audioInfo = malloc(sizeof(AudioIndexSection));
     fread(audioInfo, sizeof(AudioIndexSection), 1, file);
     assert(!memcmp(audioInfo, "AUDIINDX", 8));
@@ -27,41 +29,51 @@ Nus3audioFile* parse_file(FILE* file){
         fread(&temp, sizeof(SectionHeader), 1, file);
         //printf("%.4s\n", temp.magic);
         if(!memcmp(&temp, "TNID", 4)){
+            // Read the track ids
             trackIdSection = malloc(sizeof(TrackNumberIdSection));
             memcpy(trackIdSection, &temp, sizeof(SectionHeader));
             trackIdSection->data = malloc((size_t)temp.size);
             fread(trackIdSection->data, (size_t)temp.size, 1, file);
         }
         else if(!memcmp(&temp, "NMOF", 4)){
+            // Read the name pointers
             trackNameSection = malloc(sizeof(NameOfSection));
             memcpy(trackNameSection, &temp, sizeof(SectionHeader));
             trackNameSection->data = malloc((size_t)temp.size);
             fread(trackNameSection->data, (size_t)temp.size, 1, file);
         }
         else if(!memcmp(&temp, "ADOF", 4)){
+            // Read the file address/size information
             addressSection = malloc(sizeof(AddressSection));
             memcpy(addressSection, &temp, sizeof(SectionHeader));
             addressSection->data = malloc((size_t)temp.size);
             fread(addressSection->data, (size_t)temp.size, 1, file);
         }
         else if(!memcmp(&temp, "TNNM", 4)){
+            // Read packed string section and store the address of it in [stringoffset]
+            // to use for pointer arithmatic later
             stringOffset = ftell(file);
             stringData = malloc(temp.size);
             fread(stringData, (size_t)temp.size, 1, file);
         }
         else if(!memcmp(&temp, "JUNK", 4)){
+            // Skip file padding
             fseek(file, temp.size, SEEK_CUR);
         }
         else if(!memcmp(&temp, "PACK", 4)){
+            // Read packed file data, storing the address in [packOffset] for
+            // the purpose of pointer arithmatic later
             packOffset = ftell(file);
             packData = malloc(temp.size);
             fread(packData, (size_t)temp.size, 1, file);
             break; 
         }
     }
+    // Assert all the necessary sections exist, if they don't then rip
     assert(addressSection && trackNameSection && trackIdSection && stringData);
     Nus3audioFile* nus3audioFile = malloc(sizeof(Nus3audioFile));
     nus3audioFile->entryCount = trackCount;
+    // Convert all the data into a linked list grouped by audio file
     if(trackCount)
         nus3audioFile->head = malloc(sizeof(AudioFile));
     AudioFile* current;
@@ -74,8 +86,10 @@ Nus3audioFile* parse_file(FILE* file){
             current = current->next;
         }
         current->id = trackIdSection->trackNumbers[i];
+        // Use pointer arithmatic to point to the string in the bulk read string section
         current->name = stringData + (trackNameSection->nameOffsets[i] - stringOffset);
         current->filesize = addressSection->entries[i].fileSize;
+        // Use pointer arithmatic to point to the file in the bulk read file section
         current->data = packData + (addressSection->entries[i].fileOffset - packOffset);
     }
 
